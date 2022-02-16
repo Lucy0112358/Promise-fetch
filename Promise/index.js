ca560b1facf3d5ed0bc4f12cc5b0e31eb1c56c6c
Lusine Sahakyan <62990583+Lucy0112358@users.noreply.github.com>
2022-02-17 01:05:10 +0400

    Feat: Added files for promise
    Created and added files

---
+++ Promise/index.js
@@ -0,0 +1,24 @@
+let divForCountry = document.querySelectorAll("country")[0];
+const requestOne = `https://restcountries.com/v3.1/name/peru`;
+function makeRequestToUnsplash(requestUrl) {
+  fetch(requestUrl)
+    .then((res) => res.json())
+    .then((data) => {
+      data.forEach((countryObj) => {
+        console.log(countryObj);
+        createCountry(countryObj);
+      });
+    });
+}
+function createCountry(countryObj) {
+  const countryDiv = document.createElement("div");
+  const span = document.createElement("span");
+  span.textContent = countryObj.capital;
+  span.style.margin = "20px";
+  span.style.width = "600px";
+  span.style.height = "500px";
+  span.style.border = "double 4px black";
+  countryDiv.append(span);
+  divForCountry.append(countryDiv);
+}
+console.log(makeRequestToUnsplash(requestOne));
